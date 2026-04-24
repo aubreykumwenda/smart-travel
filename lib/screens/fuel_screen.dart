640ed72fb09ec0a909b090190bd8d13e730d2188
aubreykumwenda <aubreykumwenda5@gmail.com>
2026-04-24 09:13:02 +0200

    Create fuel_screen.dart file

---
+++ lib/screens/fuel_screen.dart
@@ -0,0 +1,17 @@
+// Fuel & roadside help screen
+
+import 'package:flutter/material.dart';
+
+class FuelScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fuel & Roadside Help'),
+      ),
+      body: Center(
+        child: Text('Fuel & Roadside Help Screen'),
+      ),
+    );
+  }
+}
